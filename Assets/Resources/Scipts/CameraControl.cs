f960ffcda7c446b6cf80ae4d09274cdb51e82405
FireDemon2903 <jona015v@edu.rybners.dk>
2024-01-03 13:36:52 +0100

    controlls semi work
    no rotation

--- Assets/Resources/Scipts/CameraControl.cs
+++ Assets/Resources/Scipts/CameraControl.cs
@@ -1,18 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CameraControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float Speed = 8.0f;
+
+    public Vector3 inp;
+
+    void OnMove(InputValue value)
     {
-        
+        inp = value.Get<Vector3>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        var step = Speed * Time.deltaTime; // calculate distance to move
+        transform.position = Vector3.MoveTowards(transform.position, transform.position+inp, step);
     }
+
+
 }
