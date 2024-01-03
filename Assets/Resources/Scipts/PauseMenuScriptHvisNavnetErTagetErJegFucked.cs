58204b07d5f0f05cf7b677804a697f77e6f55a29
casp137b <113518051+casp137b@users.noreply.github.com>
2024-01-03 15:41:37 +0100

    Jeg tror du finder mig ved træet der omme
    undskyld mig kan du find min mor, hun er blevet væk i bilka

--- Assets/Resources/Scipts/PauseMenuScriptHvisNavnetErTagetErJegFucked.cs
+++ Assets/Resources/Scipts/PauseMenuScriptHvisNavnetErTagetErJegFucked.cs
@@ -4,6 +4,7 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PauseMenuScriptHvisNavnetErTagetErJegFucked : MonoBehaviour
 {
@@ -11,6 +12,7 @@ public class PauseMenuScriptHvisNavnetErTagetErJegFucked : MonoBehaviour
     public GameObject vicMenuen;
     public TextMeshProUGUI titleText;
     [SerializeField] public bool is_paused = false;
+    bool mom_i_am_winning = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,6 +28,7 @@ public class PauseMenuScriptHvisNavnetErTagetErJegFucked : MonoBehaviour
     public void pauseDims()
     {
         is_paused = true;
+        pauseMenuen.SetActive(is_paused);
         Time.timeScale = 0;
     }
 
@@ -38,6 +41,7 @@ public class PauseMenuScriptHvisNavnetErTagetErJegFucked : MonoBehaviour
 
     public void vicMenu()
     {
+        mom_i_am_winning = true;
         Time.timeScale = 0;
         vicMenuen.SetActive(true);
         int dims = SceneManager.GetActiveScene().buildIndex;
@@ -46,9 +50,22 @@ public class PauseMenuScriptHvisNavnetErTagetErJegFucked : MonoBehaviour
 
     public void nextHole()
     {
+        mom_i_am_winning = false;
         Time.timeScale = 1;
         vicMenuen.SetActive(false);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    void OnThePauseActionTakeingDims(InputValue value)
+    {
+        if (!is_paused && !mom_i_am_winning)
+        {
+            pauseDims();
+        }
+        else if (!mom_i_am_winning && is_paused)
+        {
+            unPauseDims();
+        }
     }
 
     public void Quit()
