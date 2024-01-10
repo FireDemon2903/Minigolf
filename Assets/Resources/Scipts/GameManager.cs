45818d370cad194d59d558065d1b5133a9f4b207
FireDemon2903 <jona015v@edu.rybners.dk>
2024-01-10 14:32:20 +0100

    teleport to holes tagged "hole"

--- Assets/Resources/Scipts/GameManager.cs
+++ Assets/Resources/Scipts/GameManager.cs
@@ -1,17 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using System.Linq;
+using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    Transform[] StartPlace;
-    Transform Holes;
-
+    GameObject Player;
+    Transform[] Holes;
+    int CurrentHole = 0;
 
     private void Start()
     {
-        StartPlace = GameObject.FindGameObjectsWithTag("Hole").Select(x => x.transform).ToArray();
-        print(StartPlace.Length);
+        Player = GameObject.FindWithTag("Player");
+
+        Holes = GameObject.FindGameObjectsWithTag("Hole").Select(x => x.transform).ToArray();
     }
+
+    private void Update()
+    {
+        // For testing; not important
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            NextHole();
+        }
+    }
+
+    void NextHole()
+    {
+        ToHole(CurrentHole);
+        CurrentHole++;
+    }
+
+    void ToHole(int hole)
+    {
+        Player.transform.position = Holes[hole].position;
+    }
+
+    void Quit() { Application.Quit(); }
 }
