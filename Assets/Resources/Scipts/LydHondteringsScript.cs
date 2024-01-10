cdb26cfe055a1cf08d0a2090522d15982745efdb
casp137b <113518051+casp137b@users.noreply.github.com>
2024-01-10 12:26:53 +0100

    Slider kan nu opdatere en værdi
    lyd dims idk hvem sprugte mig

--- Assets/Resources/Scipts/LydHondteringsScript.cs
+++ Assets/Resources/Scipts/LydHondteringsScript.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class LydHondteringsScript : MonoBehaviour
 {
+    public TextMeshProUGUI volumeLabel;
+    public Slider slider;
+    public float volume;
     // Start is called before the first frame update
     void Start()
     {
-        
+        volume = slider.value;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void dasSliderValchange()
+    {
+        volume = slider.value;
+
+        volumeLabel.text = $"Lydstyrke: {Mathf.Round(volume * 100f)}";
     }
 }
